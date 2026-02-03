30ca529ba0dde5a0c4cd0636e1eecb814be99ae9
AJ Hopkins <ahopki31@ut.utm.edu>
2026-02-02 18:41:05 -0600

    added product funftion

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@ int sum(int n) {
     for(int i = 1; i <= n; i++) total += i;
     return total;
 }
+int product(int n) {
+    int total = 1;
+    for(int i = 1; i <= n; i++) total *= i;
+    return total;
+}
 int main() {
     int n;
     std::cout << "Enter n: ";
     std::cin >> n;
     std::cout << "Sum: " << sum(n) << std::endl;
+    std::cout << "Product: " << product(n) << std::endl; 
     return 0;
 }
