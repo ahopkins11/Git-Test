5093dcad84d749138416d37bf5d636afc8f228bb
AJ Hopkins <ahopki31@ut.utm.edu>
2026-02-02 18:27:33 -0600

    added sum function and gitignore

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-int main() {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
+int sum(int n) {
+    int total = 0;
+    for(int i = 1; i <= n; i++) total += i;
+    return total;
 }
-
+int main() {
+    int n;
+    std::cout << "Enter n: ";
+    std::cin >> n;
+    std::cout << "Sum: " << sum(n) << std::endl;
+    return 0;
+}
